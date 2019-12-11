629fe62675ed8f0c32e7292825f6d4f04f6e6818
Joel Chang <42378287+joelczk@users.noreply.github.com>
2019-12-11 20:34:51 +0800

    Update translate.py

--- translate.py
+++ translate.py
@@ -14,6 +14,15 @@ def start(update, context):
   message = "WELCOME " + update.message.from_user.first_name
   context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
+def check_lang(update,context):
+  user = update.message.from_user.username
+  try:
+    send_message = "Current language is set to " + user_preference[user].upper()
+    context.bot.send_message(chat_id = update.effective_chat.id, text = send_message)
+  except:
+    send_message = "No language has been set yet"
+    context.bot.send_message(chat_id = update.effective_chat.id, text = send_message)
+
 def set_preferences(update,context):
   user = update.message.from_user.username
   msg = update.message.text
@@ -28,7 +37,7 @@ def set_preferences(update,context):
       user_preference[user] = msg
     except:
       user_preference[user] = msg
-    text_message = str(msg) + " set for " + update.message.from_user.first_name
+    text_message = str(msg.upper()) + " set for " + update.message.from_user.first_name
     context.bot.send_message(chat_id=update.effective_chat.id, text = text_message)
 
 def translate(update,context):
@@ -52,7 +61,7 @@ def unknown(update, context):
   context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
 def check_language(update,context):
-    print("service started")
+
     msg = update.message.text
     msg = msg[6:]
     if len(msg) == 0:
@@ -65,13 +74,15 @@ def check_language(update,context):
       context.bot.send_message(chat_id = update.effective_chat.id, text = send_message)
 
 def main():
-  bot = telegram.Bot(token='<token>')
-  updater = Updater(token='<token>', use_context = True)
+  bot = telegram.Bot(token=<token>)
+  updater = Updater(token=<token>, use_context = True)
   dispatcher = updater.dispatcher
+  print('bot started')
   dispatcher.add_handler(CommandHandler("start", start, pass_args = True))
   dispatcher.add_handler(CommandHandler("translate", translate, pass_args = True))
   dispatcher.add_handler(CommandHandler("check", check_language, pass_args = True))
   dispatcher.add_handler(CommandHandler("lang", set_preferences, pass_args = True))
+  dispatcher.add_handler(CommandHandler("checklang", check_lang, pass_args = True))
   dispatcher.add_handler(MessageHandler(Filters.command, unknown))
   updater.start_polling()
   updater.idle()
